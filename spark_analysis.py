5d2db8bff4bd6fcb9a093a7c53a208033d03490e
Fabrizio <fabrizio.orsini04@gmail.com>
2026-05-07 16:04:07 +0200

    3.1 query analysis

--- spark_analysis.py
+++ spark_analysis.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count, avg, min, max, sum, collect_set
+from pyspark.sql.functions import *
 
 # SPARK CORE: LOCALE
 def local_analysis(filepath):
@@ -15,19 +15,26 @@ def local_analysis(filepath):
         inferSchema=True
     )
 
-
-    # result_3_1 = spark_df.groupBy("op_unique_carrier", "origin") \
-    # .agg(
-    #     count("*").alias("num_flights"),
-    #     min("arr_delay").alias("min_arr_delay"),
-    #     max("arr_delay").alias("max_arr_delay"),
-    #     avg("arr_delay").alias("avg_arr_delay"),
-    #     (sum("cancelled") / count("*")).alias("cancellation_rate"),
-    #     collect_set("month").alias("months_active")
-    # )
-
-    # result_3_1.show(20, truncate=False)
-
     # Controllo
-    spark_df.show(10)
-    spark_df.printSchema()
+    # spark_df.show(10)
+    # spark_df.printSchema()
+
+
+    # trasformazione della colonna arr_delay: se il valore è negativo viene portato a 0
+    spark_df = spark_df \
+        .withColumn("arr_delay", greatest(col("arr_delay"), lit(0))) \
+        .withColumn("dep_delay", greatest(col("dep_delay"), lit(0)))
+
+
+    result_3_1 = spark_df.groupBy("op_unique_carrier", "origin") \
+    .agg(
+        count("*").alias("num_flights"),
+        min("arr_delay").alias("min_arr_delay"),
+        max("arr_delay").alias("max_arr_delay"),
+        avg("arr_delay").alias("avg_arr_delay"),
+        (sum("cancelled") / count("*")).alias("cancellation_rate"),
+        collect_set("month").alias("months_active")
+    )
+
+    result_3_1.show(20, truncate=False)
+
