293098b99177c66927fd4f406ffa7ffdf7ed2b6c
silviatommaso <silvia.tommaso18112002@gmail.com>
2026-05-26 17:30:10 +0200

    uploading project cluster

--- analysis_3_1_cluster.py
+++ analysis_3_1_cluster.py
@@ -14,11 +14,8 @@ BUCKET = os.getenv("BUCKET")
 CLUSTER = os.getenv("CLUSTER_ID")
 
 
-def upload_project(input_file, mapper_file, reducer_file):
-
-    # INPUT
-    input_key = f"{CLUSTER}/input/{input_file.split('/')[-1]}"
-    s3.upload_file(input_file, BUCKET, input_key)
+# upload mapper e reducer nel bucket AWS 
+def upload_mapper_reducer(mapper_file, reducer_file):
 
     # MAPPER
     mapper_key = f"{CLUSTER}/code/mapper.py"
@@ -28,41 +25,83 @@ def upload_project(input_file, mapper_file, reducer_file):
     reducer_key = f"{CLUSTER}/code/reducer.py"
     s3.upload_file(reducer_file, BUCKET, reducer_key)
 
-    print("Upload completato")
+    print("Mapper e Reducer upload completato!")
 
-    return input_key, mapper_key, reducer_key
+    return mapper_key, reducer_key
 
+
+# upload input files nel bucket AWS
+def upload_input(input_file):
+
+    # INPUT
+    input_key = f"{CLUSTER}/input/{input_file.split('/')[-1]}"
+    s3.upload_file(input_file, BUCKET, input_key)
+
+    print("Input file upload completato!")
+
+    return input_key
+
+
+
+
+def upload_project():
+
+    mapper_key, reducer_key = upload_mapper_reducer("hadoop_3_1/mapper.py", "hadoop_3_1/reducer.py")
+
+    inputs = {
+        "quarter": upload_input("files/analisi_3_1_quarter.csv"),
+        #"half": upload_input("files/analisi_3_1_half.csv"),
+        #"normal": upload_input("files/analisi_3_1.csv"),
+        #"double": upload_input("files/analisi_3_1_double.csv"),
+        #"quadruple": upload_input("files/analisi_3_1_quadruple.csv"),
+    }
+
+    return mapper_key, reducer_key, inputs
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 def analysis_3_1():
-    # HADOOP AWS cluster output log
-    log_path = "output/cluster/log_hadooop_3_1.txt"
 
-    # elimina se esiste
-    if os.path.exists(log_path):
-        os.remove(log_path)
+    mapper_key, reducer_key, inputs = upload_project()
 
-    # ricrea il file (vuoto)
-    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    results = {}
 
-#   # Esecuzione Hadoop MapReduce su un quarto, metà, intera, doppia e quadrupla dimensione del file di input
+    for name, input_key in inputs.items():
 
-#   # file 1/4x
-    timer_hadoop_3_1_quarter = cluster_executor("hadoop_3_1/mapper.py", "hadoop_3_1/reducer.py", "files/analisi_3_1_quarter", None, "analisi_3_1.csv", "hadoop_3_1_output", log_path)
-    
-#   # file 1/2x
-    timer_hadoop_3_1_half = cluster_executor("hadoop_3_1/mapper.py", "hadoop_3_1/reducer.py", "files/analisi_3_1_half", None, "analisi_3_1.csv", "hadoop_3_1_output", log_path)
+        output_key = f"{CLUSTER}/output/{name}/job_3_1"
 
-#   # file 1x
-    timer_hadoop_3_1 = cluster_executor("hadoop_3_1/mapper.py", "hadoop_3_1/reducer.py", "files/analisi_3_1", "output/hadoop_3_1_output", "analisi_3_1.csv", "hadoop_3_1_output", log_path)
+        cluster_executor(
+            CLUSTER,
+            BUCKET,
+            input_key,
+            output_key,
+            mapper_key,
+            reducer_key
+        )
 
-#   # file 2x
-    timer_hadoop_3_1_double = cluster_executor("hadoop_3_1/mapper.py", "hadoop_3_1/reducer.py", "files/analisi_3_1_double", None, "analisi_3_1.csv", "hadoop_3_1_output", log_path)
+        # leggi output corretto (NO wildcard)
+        response = s3.list_objects_v2(
+            Bucket=BUCKET,
+            Prefix=output_key
+        )
 
-#   # file 4x
-    timer_hadoop_3_1_quadruple = cluster_executor("hadoop_3_1/mapper.py", "hadoop_3_1/reducer.py", "files/analisi_3_1_quadruple", None, "analisi_3_1.csv", "hadoop_3_1_output", log_path)
+        output_data = ""
 
-#   # plot dei tempi HADOOP 
-    plot.plot_analisi(timer_hadoop_3_1_quarter, timer_hadoop_3_1_half, timer_hadoop_3_1, timer_hadoop_3_1_double, timer_hadoop_3_1_quadruple, "Analisi 3.1 Hadoop AWS cluster Map Reduce", "output/cluster/hadoop_analysis_3_1.png")
+        for obj in response.get("Contents", []):
+            if "part-" in obj["Key"]:
+                file_obj = s3.get_object(
+                    Bucket=BUCKET,
+                    Key=obj["Key"]
+                )
+                output_data += file_obj["Body"].read().decode("utf-8")
+
+        results[name] = output_data
+        print(f"\n=== {name.upper()} ===")
+        print(output_data)
+
+    return results
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+analysis_3_1()
