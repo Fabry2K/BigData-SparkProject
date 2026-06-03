cf1a5fa950a1029c587ecd4c61b069dd02d8a8c2
Fabrizio <fabrizio.orsini04@gmail.com>
2026-06-03 22:51:06 +0200

    confronti spark core e sql su cluster

--- plot.py
+++ plot.py
@@ -41,3 +41,43 @@ def plot_analisi(timer_quarter, timer_half, timer_normal, timer_double, timer_qu
 
     # salvataggio
     plt.savefig(path)
+
+
+
+def plot_confronto(series, title, path):
+    """
+    Parameters
+    ----------
+    series : list of tuples
+        [
+            ("Algoritmo A", [t1, t2, t3, t4, t5]),
+            ("Algoritmo B", [t1, t2, t3, t4, t5]),
+            ...
+        ]
+    title : str
+        Titolo del grafico.
+    path : str
+        Percorso di salvataggio.
+    """
+
+    dataset_sizes = ["1/4x", "1/2x", "1x", "2x", "4x"]
+
+    plt.figure(figsize=(10, 6))
+
+    for label, execution_times in series:
+        plt.plot(
+            dataset_sizes,
+            execution_times,
+            marker="o",
+            label=label
+        )
+
+    plt.xlabel("Dataset size")
+    plt.ylabel("Execution time (seconds)")
+    plt.title(title)
+
+    plt.grid(True)
+    plt.legend()
+
+    plt.savefig(path)
+    plt.close()
